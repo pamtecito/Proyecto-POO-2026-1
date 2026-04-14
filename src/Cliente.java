930aa8062714a23a20bf02bda5bf6721f59b7fb5
SofiB-ui <sofia.bersano2501@alumnos.ubiobio.cl>
2026-04-14 12:28:04 -0400

    viaje, cliente, viaje

---
+++ src/Cliente.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Cliente {
+    private String email;
+    //ArrayList<Venta> ventas;
+
+    public Cliente(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
+    /*
+    public void addVenta(Venta venta){
+        ventas.add(venta);
+    }
+
+    public Venta[] getVentas(){
+        return ventas.toArray(new Venta[0]);
+    }
+     */
+
