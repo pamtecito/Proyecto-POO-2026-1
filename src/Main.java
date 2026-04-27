5dca9e8f20464792dab1ddea3b52f91a8389ba90
Alonso421 <alonso.gutierrez2501@alumnos.ubiobio.cl>
2026-04-26 21:24:28 -0400

    Metodo venderPasaje

--- src/Main.java
+++ src/Main.java
@@ -183,8 +183,6 @@ public class Main {
     private static void vendePasajes(Scanner tcld, SistemaVentaPasaje sistema) {
         System.out.println("...::::Venta de pasajes:::... ");
 
-
-
         System.out.println("Datos de la venta: ");
         System.out.println("ID Documento: ");
         String idDoc = tcld.next();
@@ -222,6 +220,28 @@ public class Main {
                     String bolofac = "BOLETA";
                     TipoDocumento boletaofactura = TipoDocumento.valueOf(bolofac);
                     creado = sistema.iniciaVenta(idDoc, boletaofactura, fecha, id);
+                } else {
+                    String bolofac = "Factura";
+                    TipoDocumento boletaofactura = TipoDocumento.valueOf(bolofac);
+                    creado = sistema.iniciaVenta(idDoc, boletaofactura, fecha, id);
+                }
+                System.out.println(":::: Pasajes a vender");
+                System.out.println("Cantidad de pasajes:");
+                int cantPasajes = tcld.nextInt();
+                System.out.println("Fecha de viaje:");
+                String fechaViaje = tcld.next();
+                LocalDate fechaParseada = LocalDate.parse(fechaViaje, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                String[][] horarios = sistema.getHorario(fechaParseada);
+                System.out.println(":::: Listado de horarios disponibles");
+                System.out.printf("| %-8s | %-8s | %-6s | %-9s |%n", "BUS", "SALIDA", "VALOR", "ASIENTOS");
+
+                for (int i = 0; i < horarios.length; i++) {
+                    System.out.printf("| %-8s | %-8s | %-6s | %-9s |%n",
+                            horarios[i][0],
+                            horarios[i][1],
+                            horarios[i][2],
+                            horarios[i][3]
+                    );
                 }
             }
         }
@@ -230,23 +250,192 @@ public class Main {
     }
 
     private static void venderPasaje(Scanner tcld, SistemaVentaPasaje sistema){
-        System.out.println("Datos de la venta");
-        System.out.println("ID Documento: ");
-        String id = tcld.next();
-        System.out.println("Tipo documento [1] Boleta [2] Factura: ");
-        int boletaofactura = tcld.nextInt();
-        if  (boletaofactura != 1 && boletaofactura != 2) {
-            System.out.println("No se eligio una opcion valida ([1] [2])");
-        } else {
-            System.out.println("Datos del cliente");
-            System.out.println("Rut[1] o pasaporte[2]: ");
-            int rutopasaporte = tcld.nextInt();
-            if (rutopasaporte != 1 && rutopasaporte != 2) {
-                System.out.println("No se eligio una opcion valida ([1] [2])");
-            } else {
+        System.out.println("...::::Venta de pasajes:::... ");
 
+        boolean continuar = true;
+
+        System.out.println("Datos de la venta: ");
+
+        System.out.println("ID Documento: ");
+        String idDoc = tcld.next();
+
+        System.out.println("Tipo de documento: [1]Boleta [2]Factura");
+        int tipo = tcld.nextInt();
+
+        if (tipo != 1 && tipo != 2) {
+            System.out.println("No se pudo completar. Tipo de documento invalido.");
+            continuar = false;
+        }
+
+        TipoDocumento tipoDoc = null;
+        if (continuar) {
+            if (tipo == 1) {
+                tipoDoc = TipoDocumento.BOLETA;
+            } else {
+                tipoDoc = TipoDocumento.FACTURA;
+            }
+        }
+
+
+        LocalDate fecha = null;
+        if (continuar) {
+            System.out.println("Fecha de la venta [dd/MM/yyyy]:");
+            String fechaStr = tcld.next();
+            fecha = LocalDate.parse(fechaStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        }
+
+        IdPersona id = null;
+
+        if (continuar) {
+            System.out.println("Datos del cliente:");
+            System.out.println("Rut[1] o Pasaporte[2]");
+            int opcion = tcld.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("Rut:");
+                id = Rut.of(tcld.next());
+            } else if (opcion == 2) {
+                System.out.println("Numero:");
+                String numero = tcld.next();
+                System.out.println("Nacionalidad:");
+                String nacionalidad = tcld.next();
+                id = Pasaporte.of(numero, nacionalidad);
+            } else {
+                System.out.println("Opcion invalida.");
+                continuar = false;
             }
 
+            if (id == null) {
+                System.out.println("Identificador invalido.");
+                continuar = false;
+            }
+        }
+
+        boolean creado;
+
+        if (continuar) {
+            creado = sistema.iniciaVenta(idDoc, tipoDoc, fecha, id);
+
+            if (!creado) {
+                System.out.println("No se pudo iniciar la venta.");
+                continuar = false;
+            }
+        }
+
+        int cantPasajes = 0;
+        LocalDate fechaViaje = null;
+        String[][] horariosDisponibles = null;
+
+        if (continuar) {
+            System.out.println("Cantidad de pasajes:");
+            cantPasajes = tcld.nextInt();
+
+            System.out.println("Fecha de viaje [dd/MM/yyyy]:");
+            String fechaStr = tcld.next();
+            fechaViaje = LocalDate.parse(fechaStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+            horariosDisponibles = sistema.getHorariosDisponibles(fechaViaje);
+
+            if (horariosDisponibles.length == 0) {
+                System.out.println("No hay viajes disponibles.");
+                continuar = false;
+            }
+        }
+
+        int seleccion = 0;
+        String patente = null;
+        LocalTime horaViaje = null;
+
+        if (continuar) {
+            System.out.printf("| %-3s | %-8s | %-8s | %-6s | %-9s |%n",
+                    "N°", "BUS", "SALIDA", "VALOR", "ASIENTOS");
+
+            for (int i = 0; i < horariosDisponibles.length; i++) {
+                System.out.printf("| %-3d | %-8s | %-8s | %-6s | %-9s |%n",
+                        (i + 1), horariosDisponibles[i][0], horariosDisponibles[i][1], horariosDisponibles[i][2], horariosDisponibles[i][3]);
+            }
+
+            System.out.println("Seleccione viaje en [:");
+            seleccion = tcld.nextInt();
+
+            patente = horariosDisponibles[seleccion - 1][0];
+            horaViaje = LocalTime.parse(horariosDisponibles[seleccion - 1][1]);
+        }
+
+        String[][] asientos = null;
+
+        if (continuar) {
+            asientos = sistema.listAsientosDeViaje(fechaViaje, horaViaje, patente);
+
+            if (asientos.length == 0) {
+                System.out.println("No existe el viaje.");
+                continuar = false;
+            }
+        }
+
+        if (continuar) {
+            for (int i = 0; i < asientos.length; i++) {
+                System.out.print((i + 1) + ":" + asientos[i][1] + "  ");
+            }
+            System.out.println();
+        }
+
+        if (continuar) {
+            int vendidos = 0;
+
+            while (vendidos < cantPasajes) {
+
+                System.out.println("Seleccione asiento:");
+                int asiento = tcld.nextInt();
+
+                if (asientos[asiento - 1][1].equals("*")) {
+                    System.out.println("Asiento ocupado.");
+                } else {
+
+                    System.out.println("Tipo ID pasajero [1 Rut / 2 Pasaporte]: ");
+                    int tipoPas = tcld.nextInt();
+
+                    IdPersona idPasajero = null;
+
+                    if (tipoPas == 1) {
+                        idPasajero = Rut.of(tcld.next());
+                    } else if (tipoPas == 2) {
+                        String num = tcld.next();
+                        String nac = tcld.next();
+                        idPasajero = Pasaporte.of(num, nac);
+                    }
+
+                    if (idPasajero != null) {
+
+                        boolean vendido = sistema.vendePasaje(
+                                idDoc,
+                                fechaViaje,
+                                horaViaje,
+                                patente,
+                                asiento,
+                                idPasajero
+                        );
+
+                        if (vendido) {
+                            vendidos++;
+
+                            asientos[asiento - 1][1] = "*";
+
+                        } else {
+                            System.out.println("No se pudo vender el pasaje.");
+                        }
+
+                    } else {
+                        System.out.println("Pasajero invalido.");
+                    }
+                }
+            }
+        }
+
+        if (continuar) {
+            double monto = sistema.getMontoVenta(idDoc, tipoDoc);
+            System.out.println("Venta completada.");
+            System.out.println("Monto total: " + monto);
         }
     }
 
