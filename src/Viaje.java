6ef22fdf0302f984fd266e3f5605b96d90592e00
pamtecito <pamela.aravena2501@alumnos.ubiobio.cl>
2026-04-27 15:49:53 -0400

    quito comentario y agrego String.valueOf

--- src/Viaje.java
+++ src/Viaje.java
@@ -53,18 +53,17 @@ public class Viaje {
         pasajes.add(pasaje);
     }
 
-    /*public String[][] getListaPasajeros(){
-        if (pasajes.isEmpty()) return new String[0][0];
+    public String[][] getListaPasajeros(){
         String[][] h= new String[pasajes.size()][4];
         for(int i=0; i<h.length; i++){
-            h[i][0]= pasajes.getPasajero().getIdPersona();
-            h[i][1]= pasajes.getPasajero().getNombreCompleto();
-            h[i][2]= pasajes.getPasajero().getNomContacto();
-            h[i][3]= pasajes.getPasajero().getFonoContacto();
+            h[i][0]= String.valueOf(pasajes.get(i).getPasajero().getIdPersona());
+            h[i][1]= String.valueOf(pasajes.get(i).getPasajero().getNomContacto());
+            h[i][2]= String.valueOf(pasajes.get(i).getPasajero().getNombreCompleto());
+            h[i][3]= pasajes.get(i).getPasajero().getFonoContacto();
         }
         return h;
     }
-    */
+
     public boolean existeDisponibilidad(){
         return pasajes.size() < bus.getNroAsientos();
     }
