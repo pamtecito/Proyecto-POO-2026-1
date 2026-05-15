3313084daaf098618248a10badc57a6ad4e7f41b
Alonso421 <alonso.gutierrez2501@alumnos.ubiobio.cl>
2026-05-15 12:45:48 -0400

    Cree las clases en las que debo avanzar

---
+++ src/Pago.java
@@ -0,0 +1,12 @@
+public class Pago {
+    private int monto;
+
+    public Pago(int monto) {
+        this.monto = monto;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+}
---
+++ src/Terminal.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Terminal {
+    private String nombre;
+    private Direccion direccion;
+    private ArrayList<Viaje> viajes;
+
+    public Terminal(String nombre, Direccion direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.viajes = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public void addLlegada(Viaje viaje) {
+    }
+
+    public void addSalida(Viaje viaje) {
+    }
+
+    public Viaje[] getLlegadas() {
+        return null;
+    }
+
+    public Viaje[] getSalidas() {
+        return null;
+    }
+
+
+}
