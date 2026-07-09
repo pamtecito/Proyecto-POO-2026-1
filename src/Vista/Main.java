5f9a92f9efcb051b16eb0a547a34c4318fdddd7d
pamtecito <pamela.aravena2501@alumnos.ubiobio.cl>
2026-07-09 01:36:13 -0400

    Creacion de gui y ademas menu funcionando y terminado consus respectivas acciones

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -1,6 +1,10 @@
 package Vista;
+
+import Controlador.*;
+
 public class Main {
     public static void main(String[] args) {
+        SistemaVentaPasaje.getInstance().readDatosSistema();
         UISVP.getInstance().menu();
 
     }
