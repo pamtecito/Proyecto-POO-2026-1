2585167c3ce970a8443e8a5d65584409e2c55f56
pamtecito <pamela.aravena2501@alumnos.ubiobio.cl>
2026-07-12 20:36:07 -0400

    arreglo ya avances en las GUI

--- src/Vista/GUIListLlegadasYSalidas.java
+++ src/Vista/GUIListLlegadasYSalidas.java
@@ -1,60 +1,83 @@
 package Vista;
 
+import Controlador.ControladorEmpresa;
+import Controlador.SistemaVentaPasaje;
+import Excepciones.SVPException;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
 
 public class GUIListLlegadasYSalidas extends JDialog {
     private JPanel contentPane;
-    private JButton buttonOK;
-    private JButton buttonCancel;
+    private JComboBox comboTerminal;
+    private JTextField txtTerminal;
+    private JButton buscarButton;
+    private JTable table1;
+    private JTextField txtFecha;
+    private JButton atrasButton;
+    private SistemaVentaPasaje svp;
+    private ControladorEmpresa ce;
 
     public GUIListLlegadasYSalidas() {
+        svp = SistemaVentaPasaje.getInstance();
+        ce = ControladorEmpresa.getInstance();
+
         setContentPane(contentPane);
         setModal(true);
-        getRootPane().setDefaultButton(buttonOK);
 
-        buttonOK.addActionListener(new ActionListener() {
+        buscarButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                onOK();
+                buscar();
             }
         });
 
-        buttonCancel.addActionListener(new ActionListener() {
+        atrasButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                onCancel();
+                dispose();
+                GUIMenu menu = new GUIMenu();
+                menu.setVisible(true);
             }
         });
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                onCancel();
-            }
-        });
 
-        // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
-    private void onOK() {
-        // add your code here
-        dispose();
+    private void buscar(){
+        try {
+        String nombreTerminal = txtTerminal.getText().trim();
+        if (nombreTerminal.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Debe ingresar un terminal.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        String fechaTexto = txtFecha.getText().trim();
+
+        if (fechaTexto.isEmpty()){
+            JOptionPane.showMessageDialog(this, "Debe ingresar una fecha.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        LocalDate fecha = LocalDate.parse(fechaTexto, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+
+
+        String[][] datos = ce.listLlegadasSalidasTerminal(nombreTerminal,fecha);
+
+        String[] columnaPrincipal = {"Tipo", "Hora", "Patente", "Empresa", "Ventas"};
+
+        table1.setModel(new DefaultTableModel(datos, columnaPrincipal));
+        } catch (SVPException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }catch (DateTimeParseException e){
+            JOptionPane.showMessageDialog(this, "La fecha debe tener formato dd/MM/yyyy", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
-    private void onCancel() {
-        // add your code here if necessary
-        dispose();
-    }
-
-    public static void main(String[] args) {
-        GUIListLlegadasYSalidas dialog = new GUIListLlegadasYSalidas();
-        dialog.pack();
-        dialog.setVisible(true);
-        System.exit(0);
-    }
 }
